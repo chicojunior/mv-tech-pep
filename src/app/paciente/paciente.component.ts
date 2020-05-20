30ce7bfc7e32ac71a9392a263d3446e8711eb5f0
Francisco do Vale <fdovalejr@gmail.com>
2020-05-20 10:03:16 +0200

    Mock de dados do paciente

--- src/app/paciente/paciente.component.ts
+++ src/app/paciente/paciente.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-paciente',
   templateUrl: './paciente.component.html',
-  styleUrls: ['./paciente.component.scss']
+  styleUrls: ['./paciente.component.scss'],
 })
 export class PacienteComponent implements OnInit {
+  pacienteMock = {
+    nome: 'Everardo Jose Marchezini',
+    sexo: 'masculino',
+    idade: 49,
+    convenio: 'BRADESCO / MV SAUDE',
+    especialidade: 'CARDIOLOGIA',
+    ala: 'INTERNACAO - 0002',
+    local: 'APT 602',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
